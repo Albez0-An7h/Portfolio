479df28540224171cb5a73fcf7bbb866df3f96c1
Albez0-An7h <ansh.km7096@gmail.com>
2025-04-07 10:54:38 +0530

    links fixed

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -34,7 +34,7 @@ export const projects: Project[] = [
         description: "I made this website to help my teacher keep track of GitHub activity done by the students of Newton School of Technology. This project is made in React.js with the use of the GitHub API.",
         tags: ["React", "JavaScript", "Supabase","Tailwind"],
         githubUrl: "https://github.com/Albez0-An7h/GitTales",
-        hostedUrl: "git-tales.vercel.app",
+        hostedUrl: "https://git-tales.vercel.app",
         image: "/images/GitTales.png"
 
     },
@@ -44,7 +44,7 @@ export const projects: Project[] = [
         description: "This is a clone of the Apple website built using React.js. It replicates the design and layout of Apple’s oﬃcial site, helping me practice React components, state management, and responsive UI development.",
         tags: ["React", "JavaScript", "Tailwind"],
         githubUrl: "https://github.com/Albez0-An7h/apple-clone",
-        hostedUrl: "apple-clone-omega-lovat.vercel.app",
+        hostedUrl: "https://apple-clone-omega-lovat.vercel.app",
         image: "/images/AppleClone.png"
 
     },
@@ -54,7 +54,7 @@ export const projects: Project[] = [
         description: "SmartPlanner is an intelligent task management application that leverages AI to optimize your schedule. Unlike traditional to-do apps, SmartPlanner analyzes your tasks and automatically generates an optimized daily schedule that maximizes productivity while maintaining balance. Simply add your tasks, let the AI generate an optimal schedule, and with one click, add it directly to your Google Calendar.",
         tags: ["React", "JavaScript", "Supabase","Typescript","Tailwind"],
         githubUrl: "https://github.com/Albez0-An7h/SmartPlanner",
-        hostedUrl: "smart-planner-albez0-an7h.vercel.app",
+        hostedUrl: "https://smart-planner-albez0-an7h.vercel.app",
         image: "/images/SmartPlanner.png"
     },
     
